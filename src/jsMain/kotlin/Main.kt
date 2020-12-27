f51f7576f32f5ff949efad93000d17f48664f8e9
dpavlov <dpavlov@iitdgroup.ru>
2020-12-27 21:14:21 +0700

    Собственные наработки

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,5 +1,10 @@
+import react.child
+import react.dom.render
 import kotlinx.browser.document
 
 fun main() {
-    document.getElementById("root")?.innerHTML = "Hello, Kotlin/JS!"
+    //document.getElementById("root")?.innerHTML = "Hello, Kotlin/JS! It's a Denis Pavlov's project!"
+    render(document.getElementById("root")) {
+        child(App)
+    }
 }
